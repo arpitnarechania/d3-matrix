dc0c0c77b0c11c19208e745702a39af25412aff4
Arpit Narechania <arpitnarechania@gmail.com>
2017-02-25 18:36:58 +0530

    initial commit github pages

---
+++ js/main.js
@@ -0,0 +1,60 @@
+function showLabels(element){
+    labels = element.checked;
+    drawChart();
+}
+
+function showHighlightCellOnHover(element){
+    highlightCellOnHover = element.checked;
+    drawChart();
+}
+
+function changeEndColor(element){
+    endColor = element.value;
+    drawChart();
+}
+
+function changeStartColor(element){
+    startColor = element.value;
+    drawChart();
+}
+
+function changeCellHighlightColor(element){
+    highlightCellColor = element.value;
+    drawChart();
+}
+
+function drawChart(){
+    var chartElement = document.getElementById("Matrix");
+    chartElement.innerHTML="";
+
+    var data = {
+        values: [
+            [1, 0.5, 0.1],
+            [0.2, 0.7, 0.4],
+            [0.6, 1, 0.1]
+        ],
+        labels: ['Var 1', 'Var 2','Var 3']
+    };
+
+    var chart_options = {
+            container: "#Matrix",
+            width: 400,
+            height: 400,
+            margin: {top: 50, right: 50, bottom: 100, left: 100},
+            show_labels : labels,
+            start_color : startColor,
+            end_color : endColor,
+            highlight_cell_on_hover: highlightCellOnHover,
+            highlight_cell_color: highlightCellColor
+        };
+
+    Matrix(data,chart_options); // data is served from data.js
+}
+
+var startColor = document.getElementById("startColor").value;
+var endColor = document.getElementById("endColor").value;
+var highlightCellColor = document.getElementById("highlightCellColor").value;
+var highlightCellOnHover = document.getElementById("highlightCellOnHover").checked;
+var labels = document.getElementById("labels").checked;
+
+drawChart();
